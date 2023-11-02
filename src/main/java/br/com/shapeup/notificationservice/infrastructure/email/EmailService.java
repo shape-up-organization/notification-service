eab97e2eae69db0415d4cffa880b5d794fa9d1a9
gabs <gabrieldasilvaprivado1@gmail.com>
2023-11-02 20:22:38 -0300

    update title

--- src/main/java/br/com/shapeup/notificationservice/infrastructure/email/EmailService.java
+++ src/main/java/br/com/shapeup/notificationservice/infrastructure/email/EmailService.java
@@ -5,7 +5,6 @@ import jakarta.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.buf.Utf8Encoder;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
